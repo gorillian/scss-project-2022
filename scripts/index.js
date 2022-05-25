fb7e3ae565afc98e3fefff6610cb4a4b02e964c1
Ian <ian@devpipeline.com>
2022-05-25 14:26:22 -0600

    color slider

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+const slider = document.getElementById("slider");
+
+slider.addEventListener("input", (e) => {
+  const sliderValue = e.target.value;
+  const img = document.querySelector(".foreground-img");
+
+  img.style.width = `${sliderValue}%`;
+  // console.log(sliderValue);
+});
